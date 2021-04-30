5a41aa545ea8077415fe99bee47d26c5b8069e44
Matheus <matheus@geekbusinesslab.com.br>
2021-04-30 19:39:02 -0300

    criacao de formulario

--- lib/views/sign_up_view.dart
+++ lib/views/sign_up_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SignUpView extends StatefulWidget {
   SignUpView({Key key}) : super(key: key);
 
@@ -19,30 +18,89 @@ class _SignUpViewState extends State<SignUpView> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("Cadastro")),
         automaticallyImplyLeading: false,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 300,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Nome",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Sobrenome",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Senha",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: "Data de Nascimento",
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                height: 50,
+                width: 300,
+                child: ElevatedButton(
+                  autofocus: true,
+                  child: Text(
+                    "CADASTRAR",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, "/signUp"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
     );
   }
 }
