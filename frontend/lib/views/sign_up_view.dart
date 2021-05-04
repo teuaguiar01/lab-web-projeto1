ded2d7bec9cefbce4c0778e0e0a3038d368c60e8
Alexandre <alexandrefscampos@gmail.com>
2021-05-04 20:06:49 -0300

    colocando popscop no scaffold

--- frontend/lib/views/sign_up_view.dart
+++ frontend/lib/views/sign_up_view.dart
@@ -29,124 +29,131 @@ class _SignUpViewState extends State<SignUpView> {
       }
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "Cadastro",
-            style: TextStyle(color: Colors.white),
+    _pop() async {
+      return await null;
+    }
+
+    return WillPopScope(
+      onWillPop: _pop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Cadastro",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
+          automaticallyImplyLeading: false,
         ),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 300,
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        initialValue: model.name,
-                        onChanged: (s) => model.name = s,
-                        validator: controller.isNameValid,
-                        decoration: InputDecoration(
-                          labelText: "Nome",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      TextFormField(
-                        initialValue: model.surname,
-                        onChanged: (s) => model.surname = s,
-                        validator: controller.isNameValid,
-                        decoration: InputDecoration(
-                          labelText: "Sobrenome",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      TextFormField(
-                        initialValue: model.email,
-                        onChanged: (s) => model.email = s,
-                        validator: controller.isEmailValid,
-                        decoration: InputDecoration(
-                          labelText: "Email",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      TextFormField(
-                        initialValue: model.birth,
-                        onChanged: (s) => model.birth = s,
-                        decoration: InputDecoration(
-                          labelText: "Data de Nascimento",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      TextFormField(
-                        onChanged: (s) => model.username = s,
-                        initialValue: model.username,
-                        decoration: InputDecoration(
-                          labelText: "Usuário",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      TextFormField(
-                        onChanged: (s) => model.password = s,
-                        initialValue: model.password,
-                        validator: controller.isPasswordValid,
-                        obscureText: isPasswordObscure,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              isPasswordObscure
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          initialValue: model.name,
+                          onChanged: (s) => model.name = s,
+                          validator: controller.isNameValid,
+                          decoration: InputDecoration(
+                            labelText: "Nome",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
                             ),
-                            onPressed: _toggleIsPasswordObscure,
-                          ),
-                          labelText: "Senha",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(width: 2),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 50),
+                        TextFormField(
+                          initialValue: model.surname,
+                          onChanged: (s) => model.surname = s,
+                          validator: controller.isNameValid,
+                          decoration: InputDecoration(
+                            labelText: "Sobrenome",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        TextFormField(
+                          initialValue: model.email,
+                          onChanged: (s) => model.email = s,
+                          validator: controller.isEmailValid,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        TextFormField(
+                          initialValue: model.birth,
+                          onChanged: (s) => model.birth = s,
+                          decoration: InputDecoration(
+                            labelText: "Data de Nascimento",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        TextFormField(
+                          onChanged: (s) => model.username = s,
+                          initialValue: model.username,
+                          decoration: InputDecoration(
+                            labelText: "Usuário",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        TextFormField(
+                          onChanged: (s) => model.password = s,
+                          initialValue: model.password,
+                          validator: controller.isPasswordValid,
+                          obscureText: isPasswordObscure,
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                isPasswordObscure
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                              ),
+                              onPressed: _toggleIsPasswordObscure,
+                            ),
+                            labelText: "Senha",
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 50),
-              Container(
-                height: 50,
-                width: 300,
-                child: ElevatedButton(
-                  autofocus: true,
-                  child: Text(
-                    "CADASTRAR",
-                    style: TextStyle(fontSize: 20, color: Colors.white),
+                SizedBox(height: 50),
+                Container(
+                  height: 50,
+                  width: 300,
+                  child: ElevatedButton(
+                    autofocus: true,
+                    child: Text(
+                      "CADASTRAR",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    onPressed: signUpHandle,
                   ),
-                  onPressed: signUpHandle,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
